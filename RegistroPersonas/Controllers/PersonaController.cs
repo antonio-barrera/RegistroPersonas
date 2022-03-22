8b904c212f2997a6212c6e19f9d07ddbd8f1bc15
Antonio Barrera <jeantonio.barrera@gmail.com>
2022-03-21 23:49:00 -0600

    implementacion de controlador

--- RegistroPersonas/Controllers/PersonaController.cs
+++ RegistroPersonas/Controllers/PersonaController.cs
@@ -12,6 +12,64 @@ namespace RegistroPersonas.Controllers
     [ApiController]
     public class PersonaController : ControllerBase
     {
-        
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (Models.RegistroPersonasContext db = new Models.RegistroPersonasContext())
+            {
+                var lst = (from d in db.Personas
+                           select d).ToList();
+
+                return Ok(lst);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Request.PersonaRequest model)
+        {
+            using (Models.RegistroPersonasContext db = new Models.RegistroPersonasContext())
+            {
+                Models.Persona oPersona = new Models.Persona();
+                oPersona.Nombre = model.Nombre;
+                oPersona.Direccion = model.Direccion;
+                oPersona.Correo = model.Correo;
+                oPersona.Edad = model.Edad;
+                db.Personas.Add(oPersona);
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Models.Request.PersonaEditRequest model)
+        {
+            using (Models.RegistroPersonasContext db = new Models.RegistroPersonasContext())
+            {
+                Models.Persona oPersona = db.Personas.Find(model.Id);
+                oPersona.Nombre = model.Nombre;
+                oPersona.Direccion = model.Direccion;
+                oPersona.Correo = model.Correo;
+                oPersona.Edad = model.Edad;
+                db.Entry(oPersona).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] Models.Request.PersonaEditRequest model)
+        {
+            using (Models.RegistroPersonasContext db = new Models.RegistroPersonasContext())
+            {
+                Models.Persona oPersona = db.Personas.Find(model.Id);
+                db.Personas.Remove(oPersona);
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
     }
 }
