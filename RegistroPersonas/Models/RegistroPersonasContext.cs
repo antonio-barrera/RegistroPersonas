00f3c08574e37fe341806ace79076ecc64a16bc4
Antonio Barrera <jeantonio.barrera@gmail.com>
2022-03-23 10:30:40 -0600

    se cambio la cadena de conexion de la db

--- RegistroPersonas/Models/RegistroPersonasContext.cs
+++ RegistroPersonas/Models/RegistroPersonasContext.cs
@@ -23,7 +23,7 @@ namespace RegistroPersonas.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=RegistroPersonas;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("workstation id=RegistroPersonas.mssql.somee.com;packet size=4096;user id=antoniobarrera_SQLLogin_1;pwd=vprzzaceoa;data source=RegistroPersonas.mssql.somee.com;persist security info=False;initial catalog=RegistroPersonas");
             }
         }
 
